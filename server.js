084979677b79f78f65426e084cea2d91d52d4473
brian-ambani <brainambani1@gmail.com>
2023-12-13 14:14:11 +0300

    updated the brain gpt

--- server.js
+++ server.js
@@ -1,18 +1,39 @@
-const PORT = 8000;
+require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const e = require("express");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.post("/api/ask", async (req, res) => {
+  const options = {
+    method: "POST",
+    header: {
+      "Authorization": "Bearer " + process.env.OPENAI_API_KEY,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: req.body.message}],
+      max_tokens: 100,
+    }),
+  };
+
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      options
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port " + process.env.PORT + "...");
 });
--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+const [value, setValue] = useState("");
+  const [message, setMessage] = useState("");
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const response = await fetch("http://localhost:8080/api/ask", options);
+      const data = await response.json();
+      console.log(data);
+      setMessage(data.choices[0].message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  console.log(message);
+
   return (
     <div className="app">
       <section className="side-bar">
@@ -19,8 +44,10 @@ function App() {
         <ul className="feed"></ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input />
-            <div id="submit">➢</div>
+            <input value={value} onChange={(e)=> setValue(e.target.value)}/>
+            <div id="submit" onClick={getMessages}>
+              ➢
+            </div>
           </div>
           <p className="infor">
             Brain GPT version 1.0.0. Free to use. Our goal is to make the best
